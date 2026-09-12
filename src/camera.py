925442785923811b7657f79729ec25fa5ca54d54
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2026-09-12 23:37:52 +0000

    the model performance is poor, needed a new strategy for training, instead of just training the model to recognize the sign, I'll train it based on the position of the hand as input

--- src/camera.py
+++ src/camera.py
@@ -1,13 +1,14 @@
 import cv2
+import numpy as np
+
 
 from .predict import ASLPredictor
 from .hand_detector import HandDetector
 
 
-
 def run_camera():
 
-    #predictor = ASLPredictor()
+    predictor = ASLPredictor()
     detector = HandDetector()
     camera = cv2.VideoCapture(0)
     
@@ -37,7 +38,6 @@ def run_camera():
 
             h, w, _ = frame.shape
 
-            #land mark coords
             x_coord = [
                 int(landmark.x * w)
                 for landmark in hand
@@ -48,14 +48,49 @@ def run_camera():
                 for landmark in hand
             ]
 
-            #bounding box
+            # Bounding box
             x_min = min(x_coord)
             x_max = max(x_coord)
 
             y_min = min(y_coord)
             y_max = max(y_coord)
 
-            #Draw bounding box
+            padding = 30
+
+            x_min = max(0, x_min - padding)
+            x_max = min(w, x_max + padding)
+
+            y_min = max(0, y_min - padding)
+            y_max = min(h, y_max + padding)
+
+            # Crop
+            hand_crop = frame[y_min:y_max, x_min:x_max]
+
+            # BGR -> RGB
+            rgb = cv2.cvtColor(
+                hand_crop,
+                cv2.COLOR_BGR2RGB
+            )
+
+            # CNN prediction (transform's ToPILImage handles the numpy array)
+            label, confidence = predictor.predict(rgb)
+
+            label = label[0]
+            confidence = confidence.item()
+
+            text = f"{label} ({confidence * 100:.1f}%)"
+
+            cv2.putText(
+                frame,
+                text,
+                (30, 50),
+                cv2.FONT_HERSHEY_COMPLEX,
+                1,
+                (0, 255, 0),
+                2
+            )
+
+            # Bounding box
             cv2.rectangle(
                 frame,
                 (x_min, y_min),
@@ -64,38 +99,6 @@ def run_camera():
                 2
             )
 
-            cv2.putText(
-                frame,
-                'Hand Detected',
-                (x_min, y_min - 10),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.8,
-                (0, 255, 0),
-                2
-            )
-
-        # rgb = cv2.cvtColor(
-        #     frame,
-        #     cv2.COLOR_BGR2RGB
-        # )
-        #
-        # label, confidence = predictor.predict(rgb)
-        #
-        # label = label[0]
-        # confidence = confidence.item()
-        #
-        # text = f"{label} ({confidence * 100:.1f}%)"
-        #
-        # cv2.putText(
-        #     frame,
-        #     text,
-        #     (30, 50),
-        #     cv2.FONT_HERSHEY_COMPLEX,
-        #     1,
-        #     (0, 255, 0),
-        #     2
-        # )
-        #
         cv2.imshow('ASL Recognition', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
